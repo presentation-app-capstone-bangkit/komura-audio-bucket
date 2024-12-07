475389812df8612eba3c4cade7a6c7089be8a452
Adry Mirza <adrymirza@gmail.com>
2024-12-07 10:51:47 +0700

    Update app.py to use environment variable for PORT and set host for Cloud Run

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+import os
 from flask import Flask
 from controllers.audio_controller import audio_bp  # Import blueprint
 from config import Config 
@@ -15,4 +16,6 @@ def index():
 app.register_blueprint(audio_bp, url_prefix='/audio')
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    # Gunakan PORT dari environment variable atau default ke 8080
+    port = int(os.environ.get("PORT", 8080))
+    app.run(host="0.0.0.0", port=port)  # Host harus 0.0.0.0 untuk Cloud Run
